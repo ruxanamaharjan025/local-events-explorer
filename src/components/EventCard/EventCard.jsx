89fc5b5dc330532ecfd3607fa00927eeb328d41b
ruxana maharjan <ruxanamaharjan@ruxanas-MacBook-Pro.local>
2025-07-16 11:09:02 +0100

    Polish UI for Event Card.

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import CategoryBadge from "./categoryBadge";
+import { FaMapMarkerAlt, FaCalendarAlt } from "react-icons/fa";
 
 const EventCard = ({ event }) => {
   let imageSrc = `/images/${event.image}`;
 
   return (
-    <article className="w-full h-full bg-white rounded-lg shadow-md overflow-hidden">
+    <article className="w-full h-full bg-white rounded-xl shadow-md hover:shadow-lg hover:scale-[1.01] transition-all duration-300 ease-in-out overflow-hidden border border-gray-100">
+      {/* Event Image */}
+
       <img
         src={imageSrc}
         alt={event.title}
         className="w-full h-48 object-cover rounded-t-lg"
       />
-
       <div className="p-4 flex flex-col justify-between flex-grow text-left">
-        <div className="mb-2">
+        {/* Category */}
+        <div className="mb-3">
           <CategoryBadge category={event.category} />
         </div>
-        <div className="flex justify-between my-2">
-          <time className="text-sm ">{event.date}</time>
-          <p className="text-sm ">{event.location}</p>
-        </div>
-        <h3 className="text-xl font-bold my-2">{event.title}</h3>
+        {/* Date & Location */}
+        <div className="flex items-center justify-between text-sm text-gray-600 mb-4">
+          <div className="flex items-center gap-1.5">
+            <FaCalendarAlt className=" text-green-600 text-base" />
+            <span className="text-sm ">{event.date}</span>
+          </div>
 
-        <p className="text-gray-700 line-clamp-3">{event.description}</p>
+          <div className="flex items-center gap-1.5">
+            <FaMapMarkerAlt className=" text-blue-500 text-base" />
+            <span>{event.location}</span>
+          </div>
+        </div>
+        {/* Event Title */}
+        <h3 className="text-xl md:text-xl font-semibold text-gray-800 mb-2">
+          {event.title}
+        </h3>
+        {/* Event Description */}
+        <p className="text-sm text-gray-600 text-left line-clamp-3 leading-relaxed">
+          {event.description}
+        </p>
       </div>
     </article>
   );
